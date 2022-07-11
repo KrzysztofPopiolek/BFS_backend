60483c3b558964a019361ee3141e6f82bd76dcfa
Krzysztof <kapopiolek@gmail.com>
2022-07-11 15:15:57 +0100

    adding event details endpoint

--- Controllers/EventDetailsController.cs
+++ Controllers/EventDetailsController.cs
@@ -21,7 +21,7 @@ public class EventDetailsController : ControllerBase
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<EventDetails>> Get(int id)
+    public async Task<ActionResult<EventDetails>> Get(long id)
     {
         var singleEventDetails = await _context.EventDetails.FindAsync(id);
         if (singleEventDetails == null)
@@ -29,6 +29,64 @@ public class EventDetailsController : ControllerBase
             return BadRequest("Details not found");
         }
         return Ok(singleEventDetails);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<List<EventDetails>>> Post(EventDetails newEventDetail)
+    {
+        _context.EventDetails.Add(newEventDetail);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(Get), new { id = newEventDetail.Id }, newEventDetail);
+        //return Ok(await _context.EventDetails.ToListAsync());
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutEventDetail(long id, EventDetails eventDetail)
+    {
+        if (id != eventDetail.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(eventDetail).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!EventDetailExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteEventDetail(long id)
+    {
+        var eventDetail = await _context.EventDetails.FindAsync(id);
+        if (eventDetail == null)
+        {
+            return NotFound();
+        }
+
+        _context.EventDetails.Remove(eventDetail);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool EventDetailExists(long id)
+    {
+        return _context.EventDetails.Any(e => e.Id == id);
     }
 }
 
--- EventDetails.cs
+++ EventDetails.cs
@@ -2,12 +2,12 @@ namespace BFS_backend;
 
 public class EventDetails
 {
-    public int Id  { get; set; }
+    public long? Id { get; set; }
     public DateTime? EventDate { get; set; }
     public string? EvidenceNumberName { get; set; }
     public string? ContractorName { get; set; }
     public string? ContractorAddress { get; set; }
-    public string? Account { get; set; }
+    public string? TransferType { get; set; }
     public string? TransactionType { get; set; }
     public double EventValue { get; set; }
 }
